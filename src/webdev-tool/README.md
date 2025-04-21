623df654f73df7ff36c4a210dbedeaf0ba156b7b
Derroylo <72140754+Derroylo@users.noreply.github.com>
2025-04-21 18:05:07 +0200

    Initial release

---
+++ src/webdev-tool/README.md
@@ -0,0 +1,13 @@
+# Devcontainer feature for WebDev-Tool
+
+Installs the WebDev-Tool into your devcontainer setup, using the features option
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/derroylo/devcontainers/webdev-tool:1": {
+        "version": "latest"
+    }
+}
+```
---
+++ src/webdev-tool/install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -e
+
+# Define the target directory
+TARGET_DIR="$HOME/webdev"
+
+# Define the version
+WEBDEV_VERSION=${VERSION:-latest}
+
+echo "Installing WebDev-Tool to: $TARGET_DIR"
+
+# Create the target directory if it doesn't exist
+mkdir -p "$TARGET_DIR"
+
+# Switch to target dir
+cd $TARGET_DIR
+
+# Downloading the desired version of the tool
+echo "Downloading version $WEBDEV_VERSION"
+
+curl -s https://api.github.com/repos/Derroylo/webdev-tool/releases/$WEBDEV_VERSION | grep "browser_download_url.*zip" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+
+# Unpack the webdev-tool.zip into the target directory
+unzip -o webdev-tool.zip -d "$TARGET_DIR"
+
+echo "webdev-tool.zip has been unpacked into $TARGET_DIR"
+
+# Remove the zip file
+rm webdev-tool.zip
+
+# Mark as executable
+chmod +x $HOME/webdev/webdev.sh
+
+# Add alias for bash
+echo "alias webdev='$HOME/webdev/webdev.sh'" > $HOME/.bashrc.d/webdev-tool
+
+echo "All done"
